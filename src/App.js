790c80daed84f55aaa77fb2a87a542fbbfcc0af5
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-06 16:42:54 +0530

    hashRouter

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import ShowTask from "./components/ShowTask";
 import "./App.css";
 import "./style/addtask.css";
 import "./style/showTask.css";
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Header />
-        <AddTask />
-        <ShowTask />
-      </div>
+      <HashRouter>
+        <div className="container">
+          <Header />
+          <AddTask />
+          <ShowTask />
+        </div>
+      </HashRouter>
     </div>
   );
 }
