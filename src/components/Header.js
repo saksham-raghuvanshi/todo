7db4521d1d0e5fb0214c39e9a02349518bed7b6e
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-07 19:12:20 +0530

    Theme added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../asset/logo.png";
 const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "medium"
+  );
+  //we add it html in index.html page <html class="light"></html>
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   return (
     <header>
       <div className="logo">
         <img src={logo} alt="todo Frontendgyaan logo" />
         <span>Tasktodo</span>
       </div>
-      <div className="themSelector"></div>
+      <div className="themeSelector">
+        <span
+          onClick={() => setTheme("light")}
+          className={theme === "light" ? "light activeTheme" : "light"}
+        ></span>
+        <span
+          onClick={() => setTheme("medium")}
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+        ></span>
+        <span
+          onClick={() => setTheme("dark")}
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+        ></span>
+        <span
+          onClick={() => setTheme("gOne")}
+          className={theme === "gOne" ? "gOne activeTheme" : "gOne"}
+        ></span>
+        <span
+          onClick={() => setTheme("gTwo")}
+          className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}
+        ></span>
+        <span
+          onClick={() => setTheme("gThree")}
+          className={
+            theme === "gradientThree" ? "gThree activeTheme" : "gThree"
+          }
+        ></span>
+      </div>
     </header>
   );
 };
